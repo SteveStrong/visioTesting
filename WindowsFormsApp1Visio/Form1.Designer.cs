b78aff693577880e7f4f16e593dfed0ef05f68a6
Steve Strong <sstrong@apprenticesystems.com>
2018-04-01 13:27:15 -0400

    all the code is in place

--- WindowsFormsApp1Visio/Form1.Designer.cs
+++ WindowsFormsApp1Visio/Form1.Designer.cs
@@ -28,13 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.axDrawingControl1 = new AxMicrosoft.Office.Interop.VisOcx.AxDrawingControl();
+            this.ultraTextEditor1 = new Infragistics.Win.UltraWinEditors.UltraTextEditor();
+            this.ultraButton1 = new Infragistics.Win.Misc.UltraButton();
+            ((System.ComponentModel.ISupportInitialize)(this.axDrawingControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ultraTextEditor1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axDrawingControl1
+            // 
+            this.axDrawingControl1.Enabled = true;
+            this.axDrawingControl1.Location = new System.Drawing.Point(12, 12);
+            this.axDrawingControl1.Name = "axDrawingControl1";
+            this.axDrawingControl1.Size = new System.Drawing.Size(755, 601);
+            this.axDrawingControl1.TabIndex = 0;
+            // 
+            // ultraTextEditor1
+            // 
+            this.ultraTextEditor1.Location = new System.Drawing.Point(787, 216);
+            this.ultraTextEditor1.Multiline = true;
+            this.ultraTextEditor1.Name = "ultraTextEditor1";
+            this.ultraTextEditor1.Size = new System.Drawing.Size(301, 371);
+            this.ultraTextEditor1.TabIndex = 2;
+            // 
+            // ultraButton1
+            // 
+            this.ultraButton1.Location = new System.Drawing.Point(801, 66);
+            this.ultraButton1.Name = "ultraButton1";
+            this.ultraButton1.Size = new System.Drawing.Size(75, 23);
+            this.ultraButton1.TabIndex = 3;
+            this.ultraButton1.Text = "Setup Visio";
+            this.ultraButton1.Click += new System.EventHandler(this.ultraButton1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1228, 637);
+            this.Controls.Add(this.ultraButton1);
+            this.Controls.Add(this.ultraTextEditor1);
+            this.Controls.Add(this.axDrawingControl1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axDrawingControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ultraTextEditor1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private AxMicrosoft.Office.Interop.VisOcx.AxDrawingControl axDrawingControl1;
+        private Infragistics.Win.UltraWinEditors.UltraTextEditor ultraTextEditor1;
+        private Infragistics.Win.Misc.UltraButton ultraButton1;
     }
 }
 
--- WindowsFormsApp1Visio/Form1.cs
+++ WindowsFormsApp1Visio/Form1.cs
@@ -1,20 +1,364 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Collections;
+
+using AxVisOcx = AxMicrosoft.Office.Interop.VisOcx;
+using Visio = Microsoft.Office.Interop.Visio;
 
 namespace WindowsFormsApp1Visio
 {
-    public partial class Form1 : Form
+    public enum visTF : short { TRUE = -1, FALSE = 0 };
+    public enum StencilOpenMode
     {
+        ReadOnly,
+        ReadWrite,
+        ReadHidden,
+        WriteHidden,
+        FindToClose,
+        FindToDelete
+    }
+
+    public enum DrawingEvents : short
+    {
+        QueryCancelSelectionDelete = (short)Visio.VisEventCodes.visEvtCodeQueryCancelSelDel,
+        QueryCancelMasterDelete = (short)Visio.VisEventCodes.visEvtCodeQueryCancelMasterDel,
+        QueryCancelDocumentClose = (short)Visio.VisEventCodes.visEvtCodeQueryCancelDocClose,
+        QueryCancelPageDelete = (short)Visio.VisEventCodes.visEvtCodeQueryCancelPageDel,
+        QueryCancelQuit = (short)Visio.VisEventCodes.visEvtCodeQueryCancelQuit,
+
+        //		AfterMouseDown = (short)Visio.VisEventCodes.visEvtCodeMouseDown,
+        //		AfterMouseMove = (short)Visio.VisEventCodes.visEvtCodeMouseMove,
+        //		AfterMouseUp = (short)Visio.VisEventCodes.visEvtCodeMouseUp,
+
+        AfterKeyPress = (short)Visio.VisEventCodes.visEvtCodeKeyPress,
+        AfterKeyDown = (short)Visio.VisEventCodes.visEvtCodeKeyDown,
+        AfterKeyUp = (short)Visio.VisEventCodes.visEvtCodeKeyUp,
+
+
+        AfterSelectionChanged = (short)Visio.VisEventCodes.visEvtCodeWinSelChange,
+        AfterMarker = (unchecked((short)Visio.VisEventCodes.visEvtApp) + (short)Visio.VisEventCodes.visEvtMarker),
+        AfterWindowActivate = (unchecked((short)Visio.VisEventCodes.visEvtApp) + (short)Visio.VisEventCodes.visEvtWinActivate),
+        AfterIdle = (unchecked((short)Visio.VisEventCodes.visEvtApp) + (short)Visio.VisEventCodes.visEvtNonePending),
+        BeforeTextEdit = (short)Visio.VisEventCodes.visEvtCodeShapeBeforeTextEdit,
+        AfterTextEdit = (short)Visio.VisEventCodes.visEvtCodeShapeExitTextEdit,
+        AfterTextChanged = (unchecked((short)Visio.VisEventCodes.visEvtMod) + (short)Visio.VisEventCodes.visEvtText),
+        AfterCellChanged = (unchecked((short)Visio.VisEventCodes.visEvtMod) + (short)Visio.VisEventCodes.visEvtCell),
+        AfterParentChanged = (short)Visio.VisEventCodes.visEvtCodeShapeParentChange,
+        BeforePageTurn = (short)Visio.VisEventCodes.visEvtCodeBefWinPageTurn,
+        AfterPageTurn = (short)Visio.VisEventCodes.visEvtCodeWinPageTurn,
+        AfterPageChanged = (unchecked((short)Visio.VisEventCodes.visEvtMod) + (short)Visio.VisEventCodes.visEvtPage),
+        BeforeApplicationQuit = (short)Visio.VisEventCodes.visEvtBeforeQuit,
+        AfterDocumentOpened = (unchecked((short)Visio.VisEventCodes.visEvtAdd) + (short)Visio.VisEventCodes.visEvtDoc),
+        BeforeDocumentClosed = (unchecked((short)Visio.VisEventCodes.visEvtDel) + (short)Visio.VisEventCodes.visEvtDoc),
+        AfterPageAdded = (unchecked((short)Visio.VisEventCodes.visEvtAdd) + (short)Visio.VisEventCodes.visEvtPage),
+        BeforePageDeleted = (unchecked((short)Visio.VisEventCodes.visEvtDel) + (short)Visio.VisEventCodes.visEvtPage),
+        AfterShapeAdded = (unchecked((short)Visio.VisEventCodes.visEvtAdd) + (short)Visio.VisEventCodes.visEvtShape),
+        BeforeShapeDeleted = (unchecked((short)Visio.VisEventCodes.visEvtDel) + (short)Visio.VisEventCodes.visEvtShape),
+        AfterConnectionAdded = (unchecked((short)Visio.VisEventCodes.visEvtAdd) + (short)Visio.VisEventCodes.visEvtConnect),
+        BeforeConnectionDeleted = (unchecked((short)Visio.VisEventCodes.visEvtDel) + (short)Visio.VisEventCodes.visEvtConnect),
+        BeforeSelectionDeleted = (short)Visio.VisEventCodes.visEvtCodeBefSelDel,
+        BeforeWindowSelectionDeleted = (short)Visio.VisEventCodes.visEvtCodeBefWinSelDel,
+    }
+
+
+    public partial class Form1 : Form, Visio.IVisEventProc
+    {
+        protected Visio.Application m_ovTargetApp = null;
+        protected Visio.Document m_ovTargetDoc = null;
+        protected AxVisOcx.AxDrawingControl m_ovControl = null;
+        protected Hashtable m_ovEvents = new Hashtable();
+ 
+
         public Form1()
         {
             InitializeComponent();
+            m_ovControl = axDrawingControl1;
+            ultraTextEditor1.AcceptsReturn = true;
+            m_ovControl.PageAdded += AxDrawingControl1_PageAdded;
         }
+
+        void Report(string text)
+        {
+            this.ultraTextEditor1.AppendText(text);
+            this.ultraTextEditor1.AppendText("\n");
+        }
+
+        void ReportException(Exception e)
+        {
+            Report(e.Message);
+        }
+
+        private bool StartEvents()
+        {
+            LoadDocument(@"C:\Users\Steve\Documents\visioTesting\WindowsFormsApp1Visio\sample.vsd");
+            if (m_ovTargetDoc == null)
+                return false;
+
+            Visio.EventList ovEvents = Document.EventList;
+            EstablishEvent(ovEvents, DrawingEvents.AfterShapeAdded, true);
+            EstablishEvent(ovEvents, DrawingEvents.BeforeShapeDeleted, true);
+            EstablishEvent(ovEvents, DrawingEvents.AfterParentChanged, true);
+            return true;
+        }
+        public dynamic VisEventProc(short nEventCode, object pSourceObj, int nEventID, int nEventSeqNum, object pSubjectObj, object vMoreInfo)
+        {
+             //Debug.WriteLine( string.Format("Event Code: 0x{0:X}", nEventCode));
+
+            //very critical to prevent other applications from reacting
+            //to events ment for this one.
+            Visio.Application oApp = pSourceObj as Visio.Application;
+
+
+            try
+            {
+                switch (nEventCode)
+                {
+                    case (short)DrawingEvents.BeforeApplicationQuit:
+                        break;
+                    case (short)DrawingEvents.AfterSelectionChanged:
+                        Visio.Window ovWin = pSubjectObj as Visio.Window;
+                        break;
+                    case (short)DrawingEvents.BeforeTextEdit:
+                        break;
+                    case (short)DrawingEvents.AfterTextEdit:
+                        break;
+                    case (short)DrawingEvents.AfterTextChanged:
+                        break;
+                    case (short)DrawingEvents.BeforePageTurn:
+                        break;
+                    case (short)DrawingEvents.AfterPageTurn:
+                         break;
+                    case (short)DrawingEvents.AfterPageChanged:
+                        break;
+                    case (short)DrawingEvents.AfterParentChanged:
+                        break;
+                    case (short)DrawingEvents.AfterDocumentOpened:
+                        break;
+                    case (short)DrawingEvents.BeforeDocumentClosed:
+                        break;
+                    case (short)DrawingEvents.AfterPageAdded:
+                        break;
+                    case (short)DrawingEvents.BeforePageDeleted:
+                        break;
+                    case (short)DrawingEvents.AfterShapeAdded:
+                        var m_iLastScope = m_ovTargetApp.CurrentScope;
+                        switch (m_iLastScope)
+                        {
+                            case 1166: // Duplicate from automation call shape
+                                break; // Shape should not be added
+                            case 1184: // control drag shapes
+                            case 1024: // Duplicate shape
+                            case 1017: // undo shape
+                                break;
+                            case 1018: // redo shape
+                            case 1022: // Paste shape
+                                break;
+                            case 0: // undo shape sort of
+                                break;
+                            default:
+                                break;
+                        }
+                        break;
+                    case (short)DrawingEvents.BeforeWindowSelectionDeleted:
+                        break;
+                    case (short)DrawingEvents.BeforeSelectionDeleted:
+                        m_iLastScope = m_ovTargetApp.CurrentScope;
+                        switch (m_iLastScope)
+                        {
+                            case 1023: // Delete key
+                                break;
+                            case 1020: // Cut shape
+                            case 1017: // undo shape
+                            case 1018: // redo shape
+                                break;
+                            case 1486: // deleted from page on moved
+                            default:
+                                break;
+                        }
+                        break;
+                    case (short)DrawingEvents.BeforeShapeDeleted:
+                        m_iLastScope = m_ovTargetApp.CurrentScope;
+                        break;
+                    case (short)DrawingEvents.AfterConnectionAdded:
+                        break;
+                    case (short)DrawingEvents.BeforeConnectionDeleted:
+                        break;
+                    case (short)DrawingEvents.AfterKeyPress:
+                        break;
+                    case (short)DrawingEvents.AfterKeyDown:
+                        break;
+                    case (short)DrawingEvents.AfterKeyUp:
+                        break;
+
+                    case (short)DrawingEvents.QueryCancelSelectionDelete:
+                        break;
+                    case (short)DrawingEvents.QueryCancelMasterDelete:
+                        break;
+                    case (short)DrawingEvents.QueryCancelPageDelete:
+                        break;
+                    case (short)DrawingEvents.QueryCancelDocumentClose:
+                        break;
+                    case (short)DrawingEvents.QueryCancelQuit:
+                        break;
+
+                    default:
+                        return null;
+                }
+            }
+            catch (Exception e)
+            {
+                ReportException(e);
+            }
+            return null;
+        }
+
+
+
+        private void AxDrawingControl1_PageAdded(object sender, AxVisOcx.EVisOcx_PageAddedEvent e)
+        {
+            Report(e.page.NameU);
+        }
+
+
+        public void EstablishEvent(Visio.EventList ovEventList, DrawingEvents iEvent, bool bProcess)
+        {
+            if (bProcess)
+                EnableEvent(ovEventList, iEvent);
+            else
+                DisableEvent(ovEventList, iEvent, true);
+        }
+
+        public Visio.Event EnableEvent(Visio.EventList ovEventList, DrawingEvents iEvent)
+        {
+            Visio.Event ovEvent = null;
+            string sKey = iEvent.ToString();
+
+            if (m_ovEvents.ContainsKey(sKey) == true)
+                ovEvent = m_ovEvents[sKey] as Visio.Event;
+            else
+            {
+                ovEvent = CreateEvent(ovEventList, iEvent);
+                if (ovEvent == null)
+                    return null;
+
+                m_ovEvents.Add(sKey, ovEvent);
+            }
+            ovEvent.Enabled = (short)visTF.TRUE;
+            return ovEvent;
+        }
+
+        public Visio.Event DisableEvent(Visio.EventList ovEventList, DrawingEvents iEvent, bool bRemove)
+        {
+            Visio.Event ovEvent = null;
+            string sKey = iEvent.ToString();
+
+            if (m_ovEvents.ContainsKey(sKey) == true)
+            {
+                ovEvent = m_ovEvents[sKey] as Visio.Event;
+                if (bRemove)
+                    m_ovEvents.Remove(sKey);
+
+                try
+                {
+                    ovEvent.Enabled = (short)visTF.FALSE;
+                }
+                catch { }
+            }
+            return ovEvent;
+        }
+
+
+        public Visio.Event CreateEvent(Visio.EventList ovEventList, DrawingEvents iEvent)
+        {
+            try
+            {
+                return ovEventList.AddAdvise((short)iEvent, this, "", "");
+            }
+            catch (Exception e)
+            {
+                ReportException(e);
+            }
+            return null;
+        }
+
+        public Visio.Application Application
+        {
+            get
+            {
+                try
+                {
+                    if (m_ovTargetApp == null)
+                    {
+                        m_ovTargetApp = new Visio.Application();
+                    }
+
+
+
+                    return this.m_ovTargetApp;
+                }
+                catch (Exception e)
+                {
+                    ReportException(e);
+                    return null;
+                }
+            }
+        }
+
+
+        public Visio.Document Document
+        {
+            get
+            {
+                try
+                {
+                    if (m_ovTargetDoc != null)
+                        return m_ovTargetDoc;
+
+                    m_ovTargetDoc = Application.ActiveDocument;
+                    return m_ovTargetDoc;
+                }
+                catch (Exception e)
+                {
+                    ReportException(e);
+                    return null;
+                }
+            }
+        }
+
+        private Visio.Document LoadDocument(string sPath)
+        {
+
+            Name = sPath;
+
+            try
+            {
+                m_ovControl.Src = sPath;
+            }
+            catch (Exception e)
+            {
+                ReportException(e);
+                return null;
+            }
+
+            m_ovTargetDoc = m_ovControl.Document;
+            m_ovTargetApp = m_ovTargetDoc.Application;
+
+            m_ovTargetApp.Settings.ZoomOnRoll = true;
+            m_ovTargetApp.Settings.CenterSelectionOnZoom = true;
+            m_ovTargetApp.Settings.ConnectorSplittingEnabled = true;
+            m_ovTargetApp.Settings.DrawingAids = true;
+            m_ovTargetApp.Settings.HigherQualityShapeDisplay = true;
+            m_ovTargetApp.Settings.SmoothDrawing = true;
+
+            return m_ovTargetDoc;
+        }
+
+
+        private void ultraButton1_Click(object sender, EventArgs e)
+        {
+            StartEvents();
+        }
+
+
     }
 }
